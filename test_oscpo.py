b99d19c1cf00288e05b73719db55de4b8ca83400
Tom Phillips <tomphillips@me.com>
2015-08-28 08:46:24 +0100

    Added test_eastings_northings

--- test_oscpo.py
+++ test_oscpo.py
@@ -8,3 +8,10 @@ class OscpoTest(unittest.TestCase):
         self.assertEqual(formatpostcode('W1 2AA'), 'W1  2AA')
         self.assertEqual(formatpostcode('SW7 2AZ'), 'SW7 2AZ')
         self.assertEqual(formatpostcode('WC2H 8LG'), 'WC2H8LG')
+
+    def test_eastings_northings(self):
+        e_n = eastings_northings('WC2H 8LG')
+        self.assertIsInstance(e_n, tuple)
+        self.assertEqual(len(e_n), 2)
+        self.assertIsInstance(e_n[0], int)
+        self.assertIsInstance(e_n[1], int)
